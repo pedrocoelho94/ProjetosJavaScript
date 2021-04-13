3f53fe6ee2cfbe7a3c342405e6243e9fb0b1635c
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-04-13 16:04:19 -0300

    urna finalizada

--- UrnaEletronicia/js/script.js
+++ UrnaEletronicia/js/script.js
@@ -7,12 +7,16 @@ let numeros = document.querySelector('.d-1-3')
 
 let etapaAtual = 0
 let numero = ''
+let votoBranco = false
+let votos = []
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual]
 
     let numeroHtml = ''
-    
+    numero = ''
+    votoBranco = false
+
     for(let i = 0; i < etapa.numeros; i++){
 
         if(i === 0){
@@ -50,14 +54,25 @@ function atualizaInterface(){
         
         let fotosHtml = ''
         for(let i in candidato.fotos){
+            if(candidato.fotos[i].small){
+            fotosHtml += `<div class="d-1-image small">
+                            <img src="images/${candidato.fotos[i].url}" alt="">
+                            ${candidato.fotos[i].legenda}
+                        </div>`
+            }else{
             fotosHtml += `<div class="d-1-image">
                             <img src="images/${candidato.fotos[i].url}" alt="">
                             ${candidato.fotos[i].legenda}
                         </div>`
+            }
         }
 
         lateral.innerHTML = fotosHtml
 
+    }else{
+            seuVotoPara.style.display = 'block'
+            aviso.style.display = 'block'
+            descricao.innerHTML = `<div class="aviso-Grande pisca">VOTO NULO</div>`
     }
 
     //console.log('candidato', candidato)
@@ -79,15 +94,47 @@ function clicou(n){
 }
 
 function branco(){
-    alert (`Clicou em branco`)
+    if(numero === ''){
+        votoBranco = true
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        numeros.innerHTML = ''
+        descricao.innerHTML = `<div class="aviso-Grande pisca">VOTO EM BRANCO</div>`
+    }else{
+        alert('Para votar em BRANCO, não pode ter digitado nenhum número!')
+    }
 }
 
 function corrige(){
-    alert (`Clicou em corrige`)
+    comecarEtapa()
 }
 
 function confirma(){
-    alert (`Clicou em confirma`)
+    let etapa = etapas[etapaAtual]
+    let votoConfirmado = false
+    if(votoBranco === true){
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        })
+    } else if(numero.length === etapa.numeros){
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+    }
+
+    if(votoConfirmado){
+        etapaAtual++
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa()
+        }else{
+            document.querySelector('.tela').innerHTML = `<div class="aviso-Gigante">FIM</div>`
+            console.log(votos)
+        }
+    }
 }
 
 comecarEtapa()
