e8f59869868fdc277faf0d7500491557ea24fd95
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-04-14 15:41:46 -0300

    Slider

---
+++ Slider/js/script.js
@@ -0,0 +1,41 @@
+let totalSlides = document.querySelectorAll('.sliderItem').length
+let currentSlide = 0
+
+document.querySelector('.sliderWidth').style.width =
+    `calc(100vw*${totalSlides})`
+document.querySelector('.sliderControls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`
+
+
+function goPrev(){
+    currentSlide--
+    if(currentSlide < 0){
+        currentSlide = totalSlides - 1
+    }
+    updateMargin()
+}
+
+function goNext(){
+    currentSlide++
+    if(currentSlide > (totalSlides - 1)){
+        currentSlide = 0
+    }
+    updateMargin()
+}
+
+function updateMargin(){
+    let sliderItemWidth = document.querySelector('.sliderItem').clientWidth
+    let newMargin = (currentSlide * sliderItemWidth)
+    document.querySelector('.sliderWidth').style.marginLeft = 
+        `-${newMargin}px`
+}
+
+
+function responsive(){
+    document.querySelector('.sliderControls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`
+
+    //updateMargin()
+}
+
+//setInterval(goNext, 2000)
